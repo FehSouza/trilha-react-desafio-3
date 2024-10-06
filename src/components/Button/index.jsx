95c000872bd88fd6e972204b18568dd4f16c81c9
decobot <capy@deco.cx>
2024-10-06 10:57:10 -0300

    fmt

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
+import { ButtonContainer } from './styles'
 
-import { ButtonContainer } from './styles';
-
-const Button = ({title,variant = "primary", onClick}) => {
+export const Button = ({ title, variant = 'primary', onClick }) => {
   return (
-    <ButtonContainer variant={variant} onClick={onClick}>{title}</ButtonContainer>
+    <ButtonContainer variant={variant} onClick={onClick}>
+      {title}
+    </ButtonContainer>
   )
 }
-
-export { Button }
